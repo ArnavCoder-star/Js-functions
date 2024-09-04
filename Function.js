701c2bb8c2fa164f64beaf427633c722c1372703
ArnavXXXX <arnavsinhampsvii@gmail.com>
2024-09-04 16:44:35 +0530

    second commit

---
+++ Function.js
@@ -0,0 +1,11 @@
+function mod() { // defining function
+
+    var a = 6969;
+    
+    var b = 69;
+    
+    var c = a % b;
+    
+    console.log(c);
+}
+    mod() // declaring or calling a function
---
+++ scope variable..js
@@ -0,0 +1,8 @@
+var a=69 //global variable
+function sum (){
+    var b=69
+    var c=90
+    var sum =a+b
+    console.log(sum)
+}
+sum() //calling variable
